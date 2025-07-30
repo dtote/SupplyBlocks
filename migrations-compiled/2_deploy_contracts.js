b0467b9981dc67af70136e9956e3573bfa3ea6de
Nestor Torres <neeestor08@gmail.com>
2025-07-30 23:18:04 +0100

    chore: removed unused migrations

--- migrations-compiled/2_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Manager = artifacts.require('Manager');
-module.exports = function (deployer) {
-    deployer.deploy(Manager);
-};
